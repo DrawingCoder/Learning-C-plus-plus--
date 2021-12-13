3900cf36ceae47f3cbd182274649c6e12b16a331
DrawingCoder <96078736+DrawingCoder@users.noreply.github.com>
2021-12-13 15:28:39 -0300

    Add files via upload
    The first Hello world happened december of 04, why're you seeing this? Just to suffer?

---
+++ Untitled2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+/*
+* MEU NOME É CHRISTOPHER KKKK
+* cout << "FILHO da SAMUS É O LIGHT";
+* cout << "NÃO É O KAGAMI";
+**/
+
+int main()
+{
+	//A mensagem que está entre aspas é o que estara impresso na tela
+	cout <<"Ola Mundo!";
+	return 0;
+}
